27e2128ea32f0ed7e9d9d93760b90b3960b161f4
shahedyousuf <smshahed.yousuf@gmail.com>
2022-06-13 07:41:14 +0200

    my-portfolio completed

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,15 +3,12 @@ import { HashLink } from 'react-router-hash-link';
 
 const Header = () => {
     return (
-        <div class="navbar bg-accent p-4">
+        <div class="navbar bg-accent p-4 min-w-max">
             <div class="flex-1">
                 <a class="btn btn-ghost normal-case text-xl text-white">My portfolio</a>
             </div>
             <div class="flex-none">
                 <ul class="menu menu-horizontal p-2">
-                    {/* <li><a className='text-white text-xl'>About me</a></li>
-                    <li><a className='text-white text-xl'>Projects</a></li>
-                    <li><a className='text-white text-xl'>Contact</a></li> */}
                     <li><HashLink className='text-white text-xl' smooth to='/#aboutMe'>About me</HashLink></li>
                     <li> <HashLink className='text-white text-xl' smooth to='/#projects'>Projects</HashLink></li>
                     <li><HashLink className='text-white text-xl' smooth to='/#contact'>Contact</HashLink></li>
